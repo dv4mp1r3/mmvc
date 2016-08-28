d246c04ff7b870bf9f4011694fdc686b28fa02a0
dv4mp1r3 <dv4mp1r3@192.168.0.101>
2016-08-29 01:25:49 +0300

    Добавил ViewTemplate

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 namespace app;
 
-use app\core;
+use app\core\Router;
 
 define('DEBUG', false);
 define('ROOT_DIR', dirname(__FILE__));
@@ -13,5 +13,5 @@ spl_autoload_register('app\\core\\Loader::load');
 
 $view_variable = [];
 
-$router = new core\Router();
+$router = new Router();
 $router->route();
